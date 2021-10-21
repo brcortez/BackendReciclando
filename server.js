7b857a6f14a2633770b27f09983e08a21492c0a5
Bryan Apolinario <andresbryan2806@outlook.com>
2021-10-20 22:43:24 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+
+app.get('/',(req, res) =>res.send('Backend de la app Reciclando'));
+
+app.listen(3000);
+console.log('el servidor esta corriendo en el puerto', 3000);
