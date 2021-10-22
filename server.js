bf6b59b7aef3f0c9547732ec9dcae1e06122e64a
Bryan Apolinario <andresbryan2806@outlook.com>
2021-10-22 14:04:04 -0500

    test

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/',(req, res) =>res.send('Backend de la app Reciclando'));
+app.get('/',(req, res) =>res.send('Backend de la aplicacion Reciclando'));
 
 app.listen(3000);
 console.log('el servidor esta corriendo en el puerto', 3000);
