763fd569c55100f16ff73257e9e9fbfc49c8f450
Bryan Apolinario <andresbryan2806@outlook.com>
2021-10-22 19:38:17 -0500

    prueba3

--- server.js
+++ server.js
@@ -4,16 +4,13 @@ const http = require('http');
 const server = http.createServer(app)
 
 
-const port = process.env.PORT ||3000;
+//const port = process.env.PORT ||3000;
 app.set('port',port);
 
 server.listen(3000,'147.182.169.212' || 'localhost', function(){
     console.log('aplicacion de nodeJs' + process.pid+'iniciada...')
 });
-/*
 app.get('/',(req, res) =>res.send('Backend de la aplicacion Reciclando a tiempo real'));
 
 app.listen(3000);
 console.log('test de la aplicacion en digitalOcean', 3000);
-
-*/
