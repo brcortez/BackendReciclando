d1f70cc709b4fb20c2e51e880ce376b1f2cf83a1
Bryan Apolinario <andresbryan2806@outlook.com>
2021-10-22 19:20:08 -0500

    test1

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/',(req, res) =>res.send('Backend de la aplicacion Reciclando'));
+app.get('/',(req, res) =>res.send('Backend de la aplicacion Reciclando a tiempo real'));
 
 app.listen(3000);
 console.log('test de la aplicacion en digitalOcean', 3000);
