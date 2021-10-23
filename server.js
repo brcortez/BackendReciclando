15b21a3f8506e38454f80c38926651e47d493c14
Bryan Apolinario <andresbryan2806@outlook.com>
2021-10-22 19:16:44 -0500

    test

--- server.js
+++ server.js
@@ -4,4 +4,4 @@ const app = express();
 app.get('/',(req, res) =>res.send('Backend de la aplicacion Reciclando'));
 
 app.listen(3000);
-console.log('el servidor esta corriendo en el puerto', 3000);
+console.log('test de la aplicacion en digitalOcean', 3000);
